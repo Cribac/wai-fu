237336a5ca8fc95e014c3c7062fda0234d324b73
Christian Bachhelm <cbachhelm@tuta.io>
2025-08-10 15:08:35 +0200

    Add react-aria and clsx dependencies to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import HelloWorld from '@/app/components/atoms/ui/HelloWorld.ui.component'
+
 export default function Home() {
   return (
-    <div className='font-sans'>
+    <div className="font-sans">
       <h1>Home</h1>
       <p>Whereas disregard and contempt for human rights have resulted</p>
+      <p>
+        <HelloWorld />
+      </p>
     </div>
   )
 }
